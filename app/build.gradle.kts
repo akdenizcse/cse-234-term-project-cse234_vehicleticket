787c36132f0f8bb4eb9b47fe02f54763fa1594db
kaanbsaglam <kaanberk2009@hotmail.com>
2024-05-16 20:14:10 +0300

    implement sign in screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("androidx.navigation:navigation-compose:2.4.1")
     implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
@@ -59,9 +59,10 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+//    implementation("androidx.compose.material3:material3")
     implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
+    implementation("androidx.compose.material3:material3-android:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/vehicletickettermproject/screens/signin/SignInScreen.kt
+++ app/src/main/java/com/example/vehicletickettermproject/screens/signin/SignInScreen.kt
@@ -1,8 +1,69 @@
 package com.example.vehicletickettermproject.screens.signin
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 
 @Composable
-fun SignInScreen(){
+fun SignInScreen(navController: NavController, signInViewModel: SignInViewModel = viewModel()){
+    val email by signInViewModel.email.collectAsState()
+    val password by signInViewModel.password.collectAsState()
+    val isLoading by signInViewModel.isLoading.collectAsState()
+    val context = LocalContext.current
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        TextField(
+            value = email,
+            onValueChange = { signInViewModel.onEmailChange(it)},
+            label = { Text(text = "Email") }
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = password,
+            onValueChange = {signInViewModel.onPasswordChange(it)},
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                signInViewModel.signIn(navController as NavHostController,
+                onError = {message -> Toast.makeText(context,message, Toast.LENGTH_LONG).show() })},
+            enabled = !isLoading
+        ){
+            Text(text = "Sign in")
+        }
+
+    }
 }
