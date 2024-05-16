a403c4eb579dfe0bb067951ef78ee279ba8cac9c
kaanbsaglam <kaanberk2009@hotmail.com>
2024-05-16 20:02:17 +0300

    add SignInViewModel and link firebase

---
+++ app/src/main/java/com/example/vehicletickettermproject/screens/signin/SignInViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.vehicletickettermproject.screens.signin
+
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavHostController
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+
+class SignInViewModel() : ViewModel() {
+
+    private val firebaseAuth : FirebaseAuth = FirebaseAuth.getInstance()
+
+    private val _email = MutableStateFlow("")
+    val email: StateFlow<String> get() = _email
+
+    private val _password = MutableStateFlow("")
+    val password: StateFlow<String> get() = _password
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> get() = _isLoading
+
+    fun onEmailChange(newEmail: String) {
+        _email.value = newEmail
+    }
+
+    fun onPasswordChange(newPassword: String) {
+        _password.value = newPassword
+    }
+
+
+    fun signIn(navController:NavHostController,onError : (String) -> Unit){
+
+    }
+
+
+}
