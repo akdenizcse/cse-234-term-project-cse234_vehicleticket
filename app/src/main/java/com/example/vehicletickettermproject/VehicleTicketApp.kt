febd8095086f2247f76f69e29f9521f18b11ebf1
kaanbsaglam <kaanberk2009@hotmail.com>
2024-05-16 19:26:14 +0300

    add skeleton code for screens

---
+++ app/src/main/java/com/example/vehicletickettermproject/VehicleTicketApp.kt
@@ -0,0 +1,16 @@
+package com.example.vehicletickettermproject
+
+import androidx.compose.runtime.Composable
+
+
+enum class VehicleTicketScreens(){
+    signin,
+    signup,
+    home
+}
+
+
+@Composable
+fun VehicleTicketApp(){
+
+}
---
+++ app/src/main/java/com/example/vehicletickettermproject/screens/signup/SignUpScreen.kt
@@ -0,0 +1,9 @@
+package com.example.vehicletickettermproject.screens.signup
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun SignUpScreen(){
+
+}
