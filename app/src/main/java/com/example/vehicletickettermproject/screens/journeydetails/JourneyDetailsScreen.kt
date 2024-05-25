13cfc6f11f932ff1f457ccf48e0fca76376939ef
İbrahim Utku Adanur <133052526+iuadanur@users.noreply.github.com>
2024-05-26 01:59:10 +0300

    See Seating Plan button added and dialog box has been implemented. Also I set a background color to the page

--- app/src/main/java/com/example/vehicletickettermproject/screens/journeydetails/JourneyDetailsScreen.kt
+++ app/src/main/java/com/example/vehicletickettermproject/screens/journeydetails/JourneyDetailsScreen.kt
@@ -1,12 +1,20 @@
 package com.example.vehicletickettermproject.screens.journeydetails
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -14,6 +22,7 @@ import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.Icon
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -24,6 +33,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -40,13 +50,16 @@ fun JourneyDetailsScreen(navController: NavController, journeyId: String, homeVi
     val busJourney = busJourneys.find { it.id == journeyId }
     var showSeatingPlan by remember { mutableStateOf(false) }
 
+    val backgroundColor = colorResource(id = R.color.secondary)
+
     busJourney?.let { journey ->
 
         Column (
             modifier = Modifier
-                .fillMaxSize(),
+                .fillMaxSize()
+                .background(color = backgroundColor),
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
         ){
             TopAppBar(
                 title = { Text("JOURNEY DETAILS", color = Color.White) },
@@ -77,6 +90,15 @@ fun JourneyDetailsScreen(navController: NavController, journeyId: String, homeVi
                 Text(text = "Duration: ${journey.duration}")
                 Text(text = "Price: ${journey.price} TL")
 
+                Button(
+                    onClick = { showSeatingPlan = true },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFD24545)),
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                ) {
+                    Text(text = "See Seating Plan", color = Color.White)
+                }
+
                 Text(text = "Select one of the available seats", style = MaterialTheme.typography.headlineSmall)
                 LazyColumn(
                     modifier = Modifier.fillMaxSize()
@@ -89,6 +111,35 @@ fun JourneyDetailsScreen(navController: NavController, journeyId: String, homeVi
                     }
                 }
             }
+            if (showSeatingPlan) {
+                PhotoDialog(onDismiss = { showSeatingPlan = false })
+            }
         }
     }
+}
+@Composable
+fun PhotoDialog(onDismiss: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = {
+            Text(text = "Seating Plan", color = Color(0xFFD24545))
+        },
+        text = {
+            Image(
+                painter = painterResource(id = R.drawable.grid),
+                contentDescription = "Your photo",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Fit
+            )
+        },
+        confirmButton = {},
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("OK", color = Color(0xFFD24545))
+            }
+        },
+        containerColor = Color(0xFFE6BAA3)
+    )
 }
