43d1a29881beda05ec535fa47c7b97119f740f16
İbrahim Utku Adanur <133052526+iuadanur@users.noreply.github.com>
2024-05-26 00:42:20 +0300

    Bottom Bar has been implemented. Changed color and icons

--- app/src/main/java/com/example/vehicletickettermproject/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/vehicletickettermproject/components/BottomNavigationBar.kt
@@ -1,5 +1,7 @@
 package com.example.vehicletickettermproject.components
 
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.icons.Icons
@@ -7,14 +9,21 @@ import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.vehicletickettermproject.VehicleTicketScreens
 import com.example.vehicletickettermproject.screens.home.HomeViewModel
 import com.example.vehicletickettermproject.screens.signin.SignInViewModel
+import com.example.vehicletickettermproject.R
+
 
 @Composable
 fun BottomNavigationBar(navController: NavController, signInViewModel: SignInViewModel,homeViewModel: HomeViewModel){
@@ -26,7 +35,11 @@ fun BottomNavigationBar(navController: NavController, signInViewModel: SignInVie
 
 
 
-    BottomNavigation {
+    BottomNavigation(
+        backgroundColor = Color(0xFFD24545),
+        contentColor = Color.White,
+        modifier = Modifier.height(56.dp)
+    ){
 
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
@@ -35,25 +48,31 @@ fun BottomNavigationBar(navController: NavController, signInViewModel: SignInVie
             BottomNavigationItem(
                 selected = currentRoute == screen.name,
                 onClick = {
-                    if (screen == VehicleTicketScreens.logout) {
-                        homeViewModel.clearState()
-                        signInViewModel.signOut(navController)
-                    }
-                    else{
-                        navController.navigate(screen.name) {
-
-
-                            // this avoids multiple copies of the same destination when reselecting the same item
-                            launchSingleTop = true
-
-                        }
+                    navController.navigate(screen.name) {
+                        // this avoids multiple copies of the same destination when reselecting the same item
+                        launchSingleTop = true
                     }
                 }
                 , icon = {
                     when(screen) {
-                        VehicleTicketScreens.search -> Icon(Icons.Default.Home,contentDescription = "Home")
-                        VehicleTicketScreens.reservations -> Icon(Icons.AutoMirrored.Filled.List, contentDescription = "Reservations")
-                        VehicleTicketScreens.pastTravels -> Icon(Icons.Default.Person, contentDescription = "Profile")
+                        VehicleTicketScreens.search -> Icon(
+                            painter = painterResource(id = R.drawable.search),
+                            contentDescription = "Home",
+                            tint = Color.White,
+                            modifier = Modifier.size(24.dp)
+                        )
+                        VehicleTicketScreens.reservations -> Icon(
+                            painter = painterResource(id = R.drawable.reservations), // Replace with your ticket icon
+                            contentDescription = "Ticket",
+                            tint = Color.White,
+                            modifier = Modifier.size(24.dp) // Adjust the size as needed
+                        )
+                        VehicleTicketScreens.pastTravels -> Icon(
+                            painter = painterResource(id = R.drawable.past_travels),
+                            contentDescription = "Profile",
+                            tint = Color.White,
+                            modifier = Modifier.size(24.dp)
+                        )
                         VehicleTicketScreens.logout -> Icon(Icons.AutoMirrored.Filled.ExitToApp, contentDescription = "Logout")
                         else -> {}
                     }
