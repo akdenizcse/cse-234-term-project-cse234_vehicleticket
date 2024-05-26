34ba1841f99d347401ed1e05794de6e0e92bcc71
İbrahim Utku Adanur <133052526+iuadanur@users.noreply.github.com>
2024-05-26 19:05:14 +0300

    last commit reverted

--- app/src/main/java/com/example/vehicletickettermproject/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/vehicletickettermproject/components/BottomNavigationBar.kt
@@ -1,12 +1,22 @@
 package com.example.vehicletickettermproject.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -22,9 +32,9 @@ import com.example.vehicletickettermproject.R
 @Composable
 fun BottomNavigationBar(navController: NavController, signInViewModel: SignInViewModel,homeViewModel: HomeViewModel){
     val items = listOf(
-        VehicleTicketScreens.home,
+        VehicleTicketScreens.search,
         VehicleTicketScreens.reservations,
-        VehicleTicketScreens.profile,
+        VehicleTicketScreens.pastTravels,
     )
 
     BottomNavigation(
@@ -50,7 +60,7 @@ fun BottomNavigationBar(navController: NavController, signInViewModel: SignInVie
                     val iconTint = if (currentRoute == screen.name) Color.Gray else Color.White
 
                     when(screen) {
-                        VehicleTicketScreens.home -> Icon(
+                        VehicleTicketScreens.search -> Icon(
                             painter = painterResource(id = R.drawable.search),
                             contentDescription = "Home",
                             tint = iconTint,
@@ -62,7 +72,7 @@ fun BottomNavigationBar(navController: NavController, signInViewModel: SignInVie
                             tint = iconTint,
                             modifier = Modifier.size(24.dp) // Adjust the size as needed
                         )
-                        VehicleTicketScreens.profile -> Icon(
+                        VehicleTicketScreens.pastTravels -> Icon(
                             painter = painterResource(id = R.drawable.past_travels),
                             contentDescription = "Profile",
                             tint = iconTint,
