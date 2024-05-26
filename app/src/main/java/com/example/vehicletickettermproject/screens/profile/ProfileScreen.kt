419e65596c3208635117d5a99186fc2fe50c5e19
İbrahim Utku Adanur <133052526+iuadanur@users.noreply.github.com>
2024-05-27 01:04:03 +0300

    Past travels screen has been finished

--- app/src/main/java/com/example/vehicletickettermproject/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/vehicletickettermproject/screens/profile/ProfileScreen.kt
@@ -1,5 +1,7 @@
 package com.example.vehicletickettermproject.screens.profile
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,41 +9,68 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.vehicletickettermproject.R
 import com.example.vehicletickettermproject.components.ReservationItem
 import com.example.vehicletickettermproject.screens.home.HomeViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(navController: NavController, homeViewModel: HomeViewModel = viewModel()){
     val user by homeViewModel.user.collectAsState()
     val pastTravels by homeViewModel.pastTravels.collectAsState()
 
+    val backgroundColor = colorResource(id = R.color.secondary)
 
-    Column(
+    Column (
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .background(color = backgroundColor),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        user?.let {
-            Text(text = "Welcome, ${it.firstname} ${it.lastname}!")
-            Text(text = "Email: ${it.email}")
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Text(text = "Past Travels", style = MaterialTheme.typography.headlineSmall)
-        LazyColumn(
-            modifier = Modifier.fillMaxSize()
+        TopAppBar(
+            title = { Text(
+                text = "PAST TRAVELS",
+                style = TextStyle(
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight(800),
+                    color = Color(0xFFFFFFFF),
+                )
+            ) },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = colorResource(id = R.color.primary),
+                titleContentColor = colorResource(id = R.color.white)
+            )
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
         ) {
-            items(pastTravels) { (pastReservation, pastJourney) ->
-                ReservationItem(reservation = pastReservation, journey = pastJourney, onCancelReservation = {},false)
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(pastTravels) { (pastReservation, pastJourney) ->
+                    ReservationItem(reservation = pastReservation, journey = pastJourney, onCancelReservation = {},false)
+                }
             }
         }
     }
